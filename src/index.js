73785652cc187205be0bb779eef4158bfbcaf4b1
kozhemyakin <pctdoc@gmail.com>
2023-01-06 11:53:37 +0300

    feat: add the solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let isSearch = true;
+
+  const brackets = bracketsConfig.map(item => item.join(''));
+  
+  const reStr = bracketsConfig
+      .map(bracketsPair => bracketsPair
+      .map(bracket => '[]\^$.|?*+()'.includes(bracket) ? '\\' + bracket : bracket)
+      .join(''))
+      .join('|');
+  const re = new RegExp(reStr, 'g');
+
+  while (isSearch) {
+      str = str.replace(re, '');
+      isSearch = brackets.some(bracketPair => str.includes(bracketPair)) ? true : false;
+  }
+
+return !str;
+ 
 }
